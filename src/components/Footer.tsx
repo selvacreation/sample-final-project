ffe903d862cd125dcc91e7562b10ec6cb28697b9
selvanayagan <selvanayagan@hippovideo.io>
2023-02-17 12:13:03 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const Footer : React.FunctionComponent = ()=>{
+
+    return(
+        <div style={{textAlign: "center", margin: "20px"}}>
+            {/* <p>Developed by</p> */}
+        </div>
+    )
+}
+
+export default Footer
